340da4750ff10b7ddb69d401284945d9dc102aa8
VatsalSoni_13 <sonivatsal2001@gmail.com>
2021-03-20 13:59:52 +0530

    Added Cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+var cors = require("cors");
+app.use(cors());
 const connectDB = require("./DB/Connection");
 const User = require("./models/User");
 const userRoutes = require("./routes/user");
